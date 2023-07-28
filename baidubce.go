4acae640e753e6dcb9e8bc0252a276841559e008
UsherFall <1270329076@qq.com>
2023-07-28 23:44:22 +0800

    feat: add license, fix bceClient

--- baidubce.go
+++ baidubce.go
@@ -1,3 +1,17 @@
+// Copyright 2023 The Casdoor Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package go_sms_sender
 
 import (
@@ -8,13 +22,13 @@ import (
 	"github.com/baidubce/bce-sdk-go/services/sms/api"
 )
 
-type BceClient struct {
+type BaiduClient struct {
 	sign     string
 	template string
 	core     *sms.Client
 }
 
-func GetBceClient(accessId, accessKey, sign, template string, endpoint []string) (*BceClient, error) {
+func GetBceClient(accessId, accessKey, sign, template string, endpoint []string) (*BaiduClient, error) {
 	if len(endpoint) < 1 {
 		return nil, fmt.Errorf("missing parameter: endpoint")
 	}
@@ -24,7 +38,7 @@ func GetBceClient(accessId, accessKey, sign, template string, endpoint []string)
 		return nil, err
 	}
 
-	bceClient := &BceClient{
+	bceClient := &BaiduClient{
 		sign:     sign,
 		template: template,
 		core:     client,
@@ -33,7 +47,7 @@ func GetBceClient(accessId, accessKey, sign, template string, endpoint []string)
 	return bceClient, nil
 }
 
-func (c *BceClient) SendMessage(param map[string]string, targetPhoneNumber ...string) error {
+func (c *BaiduClient) SendMessage(param map[string]string, targetPhoneNumber ...string) error {
 	code, ok := param["code"]
 	if !ok {
 		return fmt.Errorf("missing parameter: msg code")
